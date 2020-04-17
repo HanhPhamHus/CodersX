4663811613a3dc5a61f180c5ab47ff17fc6dff10
HanhPham <44548605+HanhPhamHus@users.noreply.github.com>
2020-04-17 22:05:01 +0700

    Update array_filter.js

--- JavaScript/array_filter.js
+++ JavaScript/array_filter.js
@@ -5,3 +5,39 @@ function fiveAndGreaterOnly(arr) {
       if(item >= 5) return item
   })
 }
+
+// Given an array of numbers, return a new array that only includes the even numbers.
+function evensOnly(arr) {
+  // your code here!
+  return arr.filter(function(x){
+      if(x % 2 === 0 ) return x
+  })
+}
+/**
+ * Give a list of students, filter out non-female 
+ */
+var members = [{
+    name: 'Lan',
+    gender: 'female'
+  },
+  {
+    name: 'Linh',
+    gender: 'female'
+  },
+  {
+    name: 'Trung',
+    gender: 'male'
+  },
+  {
+    name: 'Peter',
+    gender: 'gay'
+  }
+];
+
+function filterOutFemales(members) {
+  // your code here!
+  return members.filter(function(members) {
+    if (members.gender !== 'female')
+      return members.name;
+  })
+}
