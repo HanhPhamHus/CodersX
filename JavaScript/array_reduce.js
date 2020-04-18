7c3eea6ccbf4143fa228b97baeee2a7f44521b79
HanhPham <44548605+HanhPhamHus@users.noreply.github.com>
2020-04-18 15:15:40 +0700

    Update array_reduce.js

--- JavaScript/array_reduce.js
+++ JavaScript/array_reduce.js
@@ -27,3 +27,41 @@ function arrayTitle(wishlist) {
   }, 0);
   return arr;
 }
+
+// Given an array of arrays, flatten them into a single array
+
+/**
+ * Example: 
+ * var arrays = [
+ *    ["1", "2", "3"],
+ *    [true],
+ *    [4, 5, 6]
+ *  ];
+ * 
+ * flatten(arrays) // ["1", "2", "3", true, 4, 5, 6];
+ */
+
+function flatten(arr) {
+  // viết code ở đây.
+ return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
+/**
+ * Count the occurrences of each element inside an array using reduce
+ * @params {array}
+ * @return {object}
+ * Example: 
+ * countOccurrences(['a', 'b', 'c', 'b', 'a']) // { a: 2, b: 2, c: 1 };
+}
+*/
+
+function countOccurrences(arr) {
+  // viết code ở đây.
+  return arr.reduce((newObject, key) => {
+   if (newObject[key]) 
+      key = newObject[key]++;
+   else newObject[key] = 1;
+   return newObject
+ },{});
+}
