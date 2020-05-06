de727166ddb44bca0116b6c396eb314aa53d1a34
HanhPham <44548605+HanhPhamHus@users.noreply.github.com>
2020-05-06 21:59:59 +0700

    Update and rename js_last.js to cuoi_khoa_js.js

--- JavaScript/cuoi_khoa_js.js
+++ JavaScript/cuoi_khoa_js.js
@@ -95,6 +95,102 @@ function first(array, n) {
     return array
 }
 
+///Bai 15 viết hàm tạo mới một 1 string từ n ký tự từ vị trí đầu và cuối của chuỗi cũ
+// newString("1wyg5yhd45", 2) // "1w45"
+function newString(str, n){
+// viết code ở đây.
+    var arr = str.split('');
+    
+    var res = [];
+    
+    for( var i = 0; i < n ; i++){
+        res.push(arr[i]);
+    }
+    
+    for(var j = str.length - n; j < str.length; j++){
+        res.push(arr[j]);
+    }
+    
+    return res.join('')
+}
+
+/* Bai 16 Write a function that splits an array (first argument) into groups 
+the length of size(second argument) and returns them as a two-dimensional array.
+Example
+ chunkArrayInGroups(["a", "b", "c", "d", "e"], 2) // [["a", "b"], ["c", "d"], ["e"]]
+*/
+function chunkArrayInGroups(arr, size){
+  // write code here.
+  var kq=[],j=1,temp=[];
+  
+  for(var i=0;i<arr.length;i++)
+    {
+        if(j<=size){
+            temp.push(arr[i]);
+            j++;
+        }
+        if(j>size||i==arr.length-1){
+            j=1;
+            kq.push(temp);
+            temp=[];
+        }
+    }
+    return kq;
+}
+/*Bài 17: tính tống lớn nhất của k phần tử trong mảng Write a function return maximum possible sum of some of its k consecutive numbers 
+(numbers that follow each other in order.) of a given array of positive integers
+*/
+function maxOfSumChain(arr, k) {
+  // write code here.
+  var sums = 0,
+    temp = 0;
+  for (var i = 0; i <= arr.length - k; i++) {
+    for (var j = i; j < i + k; j++)
+      temp += arr[j];
+    if (sums < temp)
+      sums = temp;
+    temp = 0;
+  }
+  return sums;
+}
+/* Bài 19
+Hãy viết một hàm để kiểm tra xem có thể sắp xếp các kí tự 
+của 1 chuỗi String cho trước thành 1 chuỗi String cho trước khác không?
+
+Input: 2 chuỗi String
+Output: True hoặc False
+
+ví dụ:
+
+Input: abc cba
+Output: True
+
+Input: abx abb
+Output: False
+*/
+
+function rearrangeChar(str1, str2) {
+// Viết code tại đây!
+var dem1=[],dem2=[];
+    for(var i=0;i<=26;i++)
+    {
+        dem1[i]=0;
+        dem2[i]=0;
+    }
+    if(str1.length!=str2.length)
+        return false;
+    for(var i=0;i<str1.length;i++){
+        dem1[str1.charCodeAt(i)-97]++;
+         dem2[str2.charCodeAt(i)-97]++;
+    }
+    for(var i=0;i<=26;i++)
+    {
+        if(dem1[i]!=dem2[i])
+            return false;
+    }
+    return true;
+}
+
 
 
 
